aae8e414354560d6d2a8738542361217be7e605a
Sivhok Chhoem <sivhokchhoem@gmail.com>
2024-12-03 22:48:47 +0700

    [Create] welcome page

--- src/views/WelcomePage.js
+++ src/views/WelcomePage.js
@@ -1,36 +1,36 @@
-import { borderRadius, Box } from "@mui/system";
-import Logo from "../components/Logo";
+import { Box } from "@mui/system";
 
 function WelcomePage() {
-    return (
-
-      <Box sx={{
-        backgroundColor: 'green',
-        minHeight: '100vh', // Ensures the green background fills the entire screen height
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Box sx={{
-          backgroundColor: 'white',
-          width: { xs: '80%', md: '400px' }, // Responsive width
+  return (
+    <Box
+      sx={{
+        backgroundColor: "green",
+        minHeight: "100vh", // Ensures the green background fills the entire screen height
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "white",
+          width: { xs: "80%", md: "400px" }, // Responsive width
           padding: 2,
           borderRadius: 2,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-          <Box
-            component="img"
-            alt="DocCast Logo"
-            sx={{ width: '50%', height: 'auto' }}
-            src="/logo.png"
-            />
-        </Box>
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          component="img"
+          alt="DocCast Logo"
+          sx={{ width: "50%", height: "auto" }}
+          src="/logo.png"
+        />
       </Box>
-    
-    );
-  }
-  
-  export default WelcomePage;
-  
+    </Box>
+  );
+}
+
+export default WelcomePage;
