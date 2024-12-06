f4ae3b65b08811233ec7119d38143817c999536b
Sivhok Chhoem <sivhokchhoem@gmail.com>
2024-12-06 12:51:23 +0700

    [Update] topbar

--- src/layout/DashboardLayout/TopBar.js
+++ src/layout/DashboardLayout/TopBar.js
@@ -8,6 +8,7 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import { Divider, Menu, MenuItem } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 const pages = [
   { title: "Home", path: "/home" },
@@ -16,6 +17,8 @@ const pages = [
   { title: "Budgets", path: "/budget" },
 ];
 export default function Topbar() {
+  const theme = useTheme()
+  const [activeMenu, setActiveMenu] = React.useState(0)
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -25,6 +28,11 @@ export default function Topbar() {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
+
+  const handleClickMenu = (idex) => {
+    setActiveMenu(idex)
+    setAnchorElNav(null);
+  }
 
   return (
     <AppBar position="fixed" sx={{ boxShadow: "none" }}>
@@ -66,7 +74,7 @@ export default function Topbar() {
             {pages.map((page, index) => (
               <MenuItem
                 key={index}
-                onClick={handleCloseNavMenu}
+                onClick={() => handleClickMenu(index)}
                 component={Link}
                 to={page.path}
                 color="inherit"
@@ -89,11 +97,18 @@ export default function Topbar() {
           {pages.map((page, index) => (
             <Button
               key={index}
-              onClick={handleCloseNavMenu}
+              onClick={() => handleClickMenu(index)}
               component={Link}
               to={page.path}
               color="inherit"
-              sx={{ padding: "12px 30px" }}
+              sx={{ 
+                fontSize: "16px",
+                lineHeight: "24px",
+                fontWeight: 600,
+                padding: "12px 30px", 
+                textTransform: "capitalize",
+                color: activeMenu === index ? theme.palette.primary.main : theme.palette.text.primary 
+              }}
             >
               {page.title}
             </Button>
@@ -108,15 +123,26 @@ export default function Topbar() {
           aria-controls="menu-appbar"
           aria-haspopup="true"
           color="inherit"
+          sx={{  p: 3 }}
         >
-          <AccountCircle />
+          <AccountCircle sx={{ color: theme.palette.text.primary }}/>
         </IconButton>
         <Divider
-          sx={{ alignSelf: "center", height: "40%", margin: "0 4px" }}
+          sx={{ alignSelf: "center", height: "40%" }}
           orientation="vertical"
           flexItem
         />
-        <Button color="inherit">Logout</Button>
+        <Button color="inherit"
+          sx={{ 
+            fontSize: "16px",
+            lineHeight: "24px",
+            fontWeight: 600,
+            textTransform: "capitalize",
+            p: 2,
+            color: theme.palette.text.primary 
+          }}>
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
